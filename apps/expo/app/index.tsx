ebbc4e27a59d066bf06d4fa3d23efbfc08c07e13
Soumyajit2825 <mondalsoumya02@gmail.com>
2024-09-06 16:07:35 +0530

    bottom navbar fixed

--- apps/expo/app/index.tsx
+++ apps/expo/app/index.tsx
@@ -1,16 +1,67 @@
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import { HomeScreen } from 'app/features/home/screen'
-import { Stack } from 'expo-router'
+import { ProfileScreen } from 'app/features/profile/profile-screen'
+import { BookmarksScreen } from 'app/features/bookmarks/bookmark-screen'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
-export default function Screen() {
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+
+function HomeStack() {
   return (
-    <>
-      <Stack.Screen
-        options={{
-          title: 'Home',
-          headerShown: false,
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="Bookmarks"
+          component={BookmarksScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer independent={true}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName
+
+            if (route.name === 'Home') {
+              iconName = 'home'
+            } else if (route.name === 'Profile') {
+              iconName = 'person'
+            } else if (route.name === 'Bookmarks') {
+              iconName = 'bookmark'
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'blue',
+          inactiveTintColor: 'gray',
+          style: {
+            backgroundColor: 'white',
+            borderTopWidth: 0,
+            elevation: 5,
+          },
+          labelStyle: {
+            fontSize: 12,
+          },
         }}
-      />
-      <HomeScreen />
-    </>
+      >
+        <Tab.Screen name="Home" component={HomeStack} options={{ headerShown: false }} />
+        <Tab.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
+        <Tab.Screen name="Bookmarks" component={BookmarksScreen} options={{ headerShown: false }} />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -5,119 +5,126 @@ import { Search, Bell } from '@tamagui/lucide-icons'
 import { colorTokens } from '@tamagui/themes'
 import { StyleSheet } from 'react-native'
 import { NewCard } from 'packages/app/components/card'
+import { TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 export function HomeScreen() {
   const theme = useTheme()
+  const navigation = useNavigation()
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <YStack f={1} backgroundColor={'#FFFFFF'} p="$5" pt="$8">
-        {/* Fixed Header Section */}
-        <YStack style={styles.fixedHeader} backgroundColor={'#FFFFFF'}>
-          <XStack ai="center" jc="space-between" mb="$4">
-            <Avatar circular size="$5">
-              <Avatar.Image
-                source={{
-                  uri: 'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcR5v-SBWNA4W3KIwVOu4KhEGgfApbflgLXWsrgtTsASqs2cw-wJsyaLoQaWc7fV8jEksnDqfvA3Bv5kco0',
-                }}
-              />
-            </Avatar>
-            <Text fontSize="$6" fontWeight="bold">
-              Breaking News
-            </Text>
-            <Bell size={24} />
-          </XStack>
-
-          <Input
-            placeholder="Find interesting news"
-            mb="$4"
-            inputMode="search"
-            borderRadius="$10"
-          />
-
-          <XStack mb="$6">
-            <ScrollView horizontal={true}>
-              <Button size="$2" style={styles.gap10}>
-                Top News
-              </Button>
-              <Button size="$2" style={styles.gap10}>
-                For You
-              </Button>
-              <Button size="$2" style={styles.gap10}>
-                Politics
-              </Button>
-              <Button size="$2" style={styles.gap10}>
-                Tech Talks
-              </Button>
-              <Button size="$2" style={styles.gap10}>
-                Cooking
-              </Button>
-            </ScrollView>
-          </XStack>
-        </YStack>
-
-        {/* Scrollable Content Section */}
-        <ScrollView
-          style={styles.scrollableContent}
-          contentContainerStyle={styles.scrollableContentContainer}
-          showsVerticalScrollIndicator={false}
-        >
-          <YStack backgroundColor={'#FFFFFF'}>
-            <Card
-              elevate
-              animation="bouncy"
-              size="$4"
-              scale={0.9}
-              hoverStyle={{ scale: 0.925 }}
-              pressStyle={{ scale: 0.875 }}
-              mb="$4"
-              padding={'$4'}
-              borderRadius="$8"
-            >
-              <XStack>
-                <Card.Header padded>
-                  <Button size="$3" theme="active">
-                    Hot Topic
-                  </Button>
-                </Card.Header>
-              </XStack>
-              <Card.Footer mb="$2" mt="$10">
-                <XStack flex={1} space gap={'$10'}>
-                  <Text color="white" fontSize="$3">
-                    34 blogs
-                  </Text>
-                  <Text color="white" fontSize="$3">
-                    1720 followers
-                  </Text>
-                </XStack>
-              </Card.Footer>
-              <Card.Footer>
-                <Text fontSize="$6" fontWeight="bold" mb="$2" color="#ffffff">
-                  Engineered-Escape coming soon ft. Parentheses labs
-                </Text>
-              </Card.Footer>
-              <Card.Background style={styles.cardBackground}>
-                <Image
-                  style={styles.cardImage}
-                  source={{
-                    uri: 'https://img.freepik.com/free-photo/realistic-stacked-books-shelf_23-2151359536.jpg',
-                  }}
-                />
-              </Card.Background>
-            </Card>
-            <XStack gap="$14">
-              <Text fontSize="$6" fontWeight="bold" mb="$4">
-                Recommended
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <YStack f={1} backgroundColor={'#FFFFFF'} p="$5" pt="$8">
+          {/* Fixed Header Section */}
+          <YStack style={styles.fixedHeader} backgroundColor={'#FFFFFF'}>
+            <XStack ai="center" jc="space-between" mb="$4">
+              <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+                <Avatar circular size="$5">
+                  <Avatar.Image
+                    source={{
+                      uri: 'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcR5v-SBWNA4W3KIwVOu4KhEGgfApbflgLXWsrgtTsASqs2cw-wJsyaLoQaWc7fV8jEksnDqfvA3Bv5kco0',
+                    }}
+                  />
+                </Avatar>
+              </TouchableOpacity>
+              <Text fontSize="$6" fontWeight="bold">
+                Breaking News
               </Text>
-              <Text fontSize="$6" fontWeight="bold" mb="$4" color="$gray10">
-                See More
-              </Text>
+              <Bell size={24} />
             </XStack>
-            <NewCard />
+
+            <Input
+              placeholder="Find interesting news"
+              mb="$4"
+              inputMode="search"
+              borderRadius="$10"
+            />
+
+            <XStack mb="$6">
+              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                <Button size="$2" style={styles.gap10}>
+                  Top News
+                </Button>
+                <Button size="$2" style={styles.gap10}>
+                  For You
+                </Button>
+                <Button size="$2" style={styles.gap10}>
+                  Politics
+                </Button>
+                <Button size="$2" style={styles.gap10}>
+                  Tech Talks
+                </Button>
+                <Button size="$2" style={styles.gap10}>
+                  Cooking
+                </Button>
+              </ScrollView>
+            </XStack>
           </YStack>
-        </ScrollView>
-      </YStack>
-    </SafeAreaView>
+
+          {/* Scrollable Content Section */}
+          <ScrollView
+            style={styles.scrollableContent}
+            contentContainerStyle={styles.scrollableContentContainer}
+            showsVerticalScrollIndicator={false}
+          >
+            <YStack backgroundColor={'#FFFFFF'}>
+              <Card
+                elevate
+                animation="bouncy"
+                size="$4"
+                scale={0.9}
+                hoverStyle={{ scale: 0.925 }}
+                pressStyle={{ scale: 0.875 }}
+                mb="$4"
+                padding={'$4'}
+                borderRadius="$8"
+              >
+                <XStack>
+                  <Card.Header padded>
+                    <Button size="$3" theme="active">
+                      Hot Topic
+                    </Button>
+                  </Card.Header>
+                </XStack>
+                <Card.Footer mb="$2" mt="$10">
+                  <XStack flex={1} space gap={'$10'}>
+                    <Text color="white" fontSize="$3">
+                      34 blogs
+                    </Text>
+                    <Text color="white" fontSize="$3">
+                      1720 followers
+                    </Text>
+                  </XStack>
+                </Card.Footer>
+                <Card.Footer>
+                  <Text fontSize="$6" fontWeight="bold" mb="$2" color="#ffffff">
+                    Engineered-Escape coming soon ft. Parentheses labs
+                  </Text>
+                </Card.Footer>
+                <Card.Background style={styles.cardBackground}>
+                  <Image
+                    style={styles.cardImage}
+                    source={{
+                      uri: 'https://img.freepik.com/free-photo/realistic-stacked-books-shelf_23-2151359536.jpg',
+                    }}
+                  />
+                </Card.Background>
+              </Card>
+              <XStack gap="$14">
+                <Text fontSize="$6" fontWeight="bold" mb="$4">
+                  Recommended
+                </Text>
+                <Text fontSize="$6" fontWeight="bold" mb="$4" color="$gray10">
+                  See More
+                </Text>
+              </XStack>
+              <NewCard />
+            </YStack>
+          </ScrollView>
+        </YStack>
+      </SafeAreaView>
+    </>
   )
 }
 
