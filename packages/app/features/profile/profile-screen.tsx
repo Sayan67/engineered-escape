045aee65e7704bc99c3d26cde0ee471727bf0d03
Soumyajit2825 <mondalsoumya02@gmail.com>
2024-09-10 10:30:49 +0530

    profile tab is created and bookmarks tab is created

--- packages/app/features/profile/profile-screen.tsx
+++ packages/app/features/profile/profile-screen.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native'
 import { XStack, YStack, Text, Avatar, Button, useTheme } from 'tamagui'
-import { StyleSheet } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
 export function ProfileScreen() {
@@ -10,18 +9,34 @@ export function ProfileScreen() {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView>
-        <YStack f={1} backgroundColor={'white'} p="$5" pt="$8">
-          <XStack ai="center" jc="space-between" mb="$4">
-            <Button onPress={() => navigation.goBack()}>Back</Button>
-            <Avatar circular size="$5">
-              <Avatar.Image
-                source={{
-                  uri: 'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcR5v-SBWNA4W3KIwVOu4KhEGgfApbflgLXWsrgtTsASqs2cw-wJsyaLoQaWc7fV8jEksnDqfvA3Bv5kco0',
-                }}
-              />
-            </Avatar>
-          </XStack>
+      <ScrollView contentContainerStyle={styles.container}>
+        <YStack
+          ai="center"
+          jc="center"
+          style={{ height: '100%', width: '100%' }}
+          backgroundColor={'white'}
+          p="$5"
+          pt="$8"
+        >
+          <Avatar circular size="$10" mb="$4">
+            <Avatar.Image
+              source={{
+                uri: 'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcR5v-SBWNA4W3KIwVOu4KhEGgfApbflgLXWsrgtTsASqs2cw-wJsyaLoQaWc7fV8jEksnDqfvA3Bv5kco0',
+              }}
+            />
+          </Avatar>
+          <Text fontSize="$6" fontWeight="bold" mb="$2">
+            Username
+          </Text>
+          <Text fontSize="$4" color="$gray10">
+            Joined on January 1, 2023
+          </Text>
+          <Text fontSize="$4" color="$gray10" mb="$2">
+            Number of Blogs Written: 10
+          </Text>
+          <Button mt="$4" onPress={() => navigation.goBack()}>
+            Back
+          </Button>
         </YStack>
       </ScrollView>
     </SafeAreaView>
@@ -29,7 +44,11 @@ export function ProfileScreen() {
 }
 
 const styles = StyleSheet.create({
-  gap10: {
-    marginRight: 10,
+  container: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%',
   },
 })
